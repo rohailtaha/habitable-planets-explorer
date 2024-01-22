8d42b0c7c8150dd96bd39f71f7771d2faba6fd1a
Rohail Taha <taharohail77@gmail.com>
2024-01-22 15:58:53 +0500

    feat: initialize project

---
+++ index.js
@@ -0,0 +1,33 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+function isHabitablePlanet(planet) {
+  return (
+    planet.koi_disposition === 'CONFIRMED' &&
+    planet.koi_insol > 0.36 &&
+    planet.koi_insol < 1.11 &&
+    planet.koi_prad < 1.6
+  );
+}
+
+const habitablePlanets = [];
+
+fs.createReadStream('./kepler_planets.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', data => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', error => {
+    console.error(error);
+  })
+  .on('end', () => {
+    console.log('Habitable planets: ');
+    console.log(habitablePlanets.map(planet => planet.kepler_name));
+  });
